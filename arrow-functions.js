1b8b8d16148f34d80c932e1797d66a4174e63b6c
mariahynes <maria.m.hynes@gmail.com>
2021-08-30 10:10:36 +0000

    Map method examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,34 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+const students = [
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skill: 'Javascript'
+    },
+    {
+        id: 2,
+        name: 'Maria',
+        profession: 'Developer',
+        skill: 'Django'
+    },
+    {
+        id: 2,
+        name: 'Barry',
+        profession: 'Engineer',
+        skill: 'Cisco'
+    },
+];
 
-// Arrow Function With Parameters
+const studentsWithIds = students.map(student => [student.id, student.name]);
+//console.log(studentsWithIds);
 
-// Single Line Arrow Function With Parameters
+const studentOjb = students.map(student => ({id: student['id'], name: student['name']}));
+//console.log(studentOjb);
 
-// Implicit Returns
-
-// Returning Multiple Lines
+const ages = [32, 26, 28];
+//console.log(age);
+const studObjwAge = students.map((student, age) => ({...student,age:ages[age]}));
+console.log(studObjwAge);
