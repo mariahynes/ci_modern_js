6aaf8823b580f1a3c68ee456998083545ad5c421
mariahynes <maria.m.hynes@gmail.com>
2021-08-30 17:12:43 +0000

    Add reduce function with grouping

--- reduce-examples.js
+++ reduce-examples.js
@@ -7,4 +7,51 @@ let sum = nums.reduce((acc, curr) => {
     );
     return acc + curr;
 },0);
-console.log(sum);
+let sums2 = nums.reduce((acc, curr) => acc + curr, 10);
+console.log(sums2);
+
+const students = [
+    {
+        name: 'Mark',
+        profession: 'Developer',
+        yrsExperience: 7
+    },
+    {
+        name: 'Maria',
+        profession: 'Developer',
+        yrsExperience: 2
+    },
+    {
+        name: 'Barry',
+        profession: 'Engineer',
+        yrsExperience: 4
+    },
+    {
+        name: 'Peter',
+        profession: 'Engineer',
+        yrsExperience: 6
+    },
+];
+//totalling a specific object property
+theTotal = students.reduce((acc,curr) => acc + curr.yrsExperience,0);
+console.log(theTotal);
+
+//grouping by a property and totalling the group:
+//decide first what the output result should look like:
+// { Developer: 9, Engineer: 10 }
+//create with 'longer-form' arrow function because this will be more than one line 
+//and will need a return statement
+//note that the first set of {} signifies the bountary of the call-back function and  
+//that the second set it setting the initial value as an empty object {}
+let experienceByProfession = students.reduce((acc, curr) => {
+    let key = curr.profession;
+    //first time we get this key we will add it into the return object
+    //if it comes up again, we will not add it
+    if(!acc[key]){
+        acc[key] = curr.yrsExperience;
+    }else{
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+},{});
+console.log(experienceByProfession);
